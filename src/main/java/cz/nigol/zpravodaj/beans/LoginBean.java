4804db9e480595a7be8a860da4151e725c05174e
Martin Polak <nigol@nigol.cz>
2018-11-26 09:10:48 +0000

    Login done.

--- src/main/java/cz/nigol/zpravodaj/beans/LoginBean.java
+++ src/main/java/cz/nigol/zpravodaj/beans/LoginBean.java
@@ -1,17 +1,43 @@
 package cz.nigol.zpravodaj.beans;
 
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 import javax.inject.Named;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+import cz.nigol.zpravodaj.entities.User;
+import cz.nigol.zpravodaj.services.UserService;
 
 @Named
 @RequestScoped
 public class LoginBean {
+    @Inject
+    private SessionBean sessionBean;
+    @Inject
+    private UserService userService;
+    @Inject
+    private FacesContext facesContext;
     private String loginName;
     private String password;
 
-    public String login() {
-	return "";
-    }	
+	public String login() {
+	String result;
+	User user = userService.getUserById(loginName);
+	if (user != null && user.isActive() && BCrypt.checkpw(password, user.getPassword())) {
+	    sessionBean.setUser(user);
+	    result = "/au/moje-clanky.xhtml?faces-redirect=true";
+	} else {
+	    sessionBean.setUser(null);
+	    facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
+							   "Chyba",  "Nesprávné přihlašovací údaje."));
+	    facesContext.getExternalContext().getFlash().setKeepMessages(true);
+	    result = "/login.xhtml?faces-redirect=true";
+	}
+	return result;
+    }
 
     /**
      * @return the loginName
