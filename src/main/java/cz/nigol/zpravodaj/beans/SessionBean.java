0eb73271ee93012c9fa21e199ed7a16a9752ae03
Martin Polak <nigol@nigol.cz>
2018-12-16 08:38:40 +0000

    Prepare user for development.

--- src/main/java/cz/nigol/zpravodaj/beans/SessionBean.java
+++ src/main/java/cz/nigol/zpravodaj/beans/SessionBean.java
@@ -2,11 +2,13 @@ package cz.nigol.zpravodaj.beans;
 
 import java.io.Serializable;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import cz.nigol.zpravodaj.dev.PrepareDevData;
 import cz.nigol.zpravodaj.entities.User;
 
 @Named
@@ -15,7 +17,14 @@ public class SessionBean implements Serializable {
     private static final long serialVersionUID = 9212583897602369531L;
     @Inject
     private FacesContext facesContext;
+    @Inject
+    private PrepareDevData prepareDevData;
     private User user = new User();
+
+    @PostConstruct
+    public void init() {
+	//prepareDevData.createData();
+    }
 
     public String logout() {
 	user = null;
---
+++ src/main/java/cz/nigol/zpravodaj/dev/PrepareDevData.java
@@ -0,0 +1,27 @@
+package cz.nigol.zpravodaj.dev;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+import cz.nigol.zpravodaj.entities.User;
+import cz.nigol.zpravodaj.enums.Role;
+import cz.nigol.zpravodaj.services.UserService;
+
+@Named
+@RequestScoped
+public class PrepareDevData {
+    @Inject
+    private UserService userService;
+
+    public void createData() {
+	User user = new User();
+	user.setId("aaa");
+	user.setFullName("Rum Bum");
+	user.setRole(Role.EDITOR);
+	user.setPassword(BCrypt.hashpw("aaa", BCrypt.gensalt()));
+	userService.saveUser(user);
+    }
+}
