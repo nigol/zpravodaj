a2ac25cfe3e1e0e2ac5486ec4eaf0eb649190f2d
Martin Polak <nigol@nigol.cz>
2018-12-30 10:18:47 +0000

    OpenGraph meta tags.

--- src/main/java/cz/nigol/zpravodaj/beans/ApplicationBean.java
+++ src/main/java/cz/nigol/zpravodaj/beans/ApplicationBean.java
@@ -6,13 +6,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
+
+import cz.nigol.zpravodaj.config.Configuration;
 
 @Named
 @ApplicationScoped
 public class ApplicationBean {
+    @Resource
+    private Configuration configuration;
     private Map<Integer, String> daysOfWeek;
+    private String url;
 
     @PostConstruct
     public void init() {
@@ -24,6 +30,7 @@ public class ApplicationBean {
 	daysOfWeek.put(5, "čtvrtek");
 	daysOfWeek.put(6, "pátek");
 	daysOfWeek.put(7, "sobota");
+	url = configuration.getUrl();
     }
 
     public Date today() {
@@ -35,5 +42,19 @@ public class ApplicationBean {
 	calendar.setTime(today());
 	int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
 	return daysOfWeek.get(dayOfWeek);
+    }
+
+    /**
+     * @return the url
+     */
+    public String getUrl() {
+	return url;
+    }
+
+    /**
+     * @param url the url to set
+     */
+    public void setUrl(String url) {
+	this.url = url;
     }
 }
