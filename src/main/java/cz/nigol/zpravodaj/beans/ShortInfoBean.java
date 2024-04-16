53382ca3e16100be9fca78e815f4e8fc6b06b747
Martin Polák <nigol@users.noreply.github.com>
2024-04-16 20:21:56 +0200

    ShortInfo WIP

--- src/main/java/cz/nigol/zpravodaj/beans/ShortInfoBean.java
+++ src/main/java/cz/nigol/zpravodaj/beans/ShortInfoBean.java
@@ -7,6 +7,8 @@ import javax.inject.*;
 import javax.enterprise.context.*;
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 
 import cz.nigol.zpravodaj.entities.*;
 import cz.nigol.zpravodaj.services.*;
@@ -17,6 +19,8 @@ public class ShortInfoBean implements Serializable {
     private static final long serialVersionUID = 9212583897602369531L;
     @Inject
     private ShortInfoService shortInfoService;
+    @Inject
+    private FacesContext facesContext;
     private List<ShortInfo> shortInfos;
     private List<ShortInfo> shortInfosToday;
     private ShortInfo shortInfo;
@@ -28,12 +32,16 @@ public class ShortInfoBean implements Serializable {
 
     public void save() {
       shortInfoService.save(shortInfo);
+      shortInfo = null;
+      init();
+      facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "", "Zpráva byla uložena."));
     }
 
     public void newShortInfo() {
       shortInfo = new ShortInfo();
       shortInfo.setCreatedAt(new Date());
       shortInfo.setInfoDate(new Date());
+      shortInfos.add(shortInfo);
     }
 
     public ShortInfo getShortInfo() {
